50a76edc1b6a25f01d0fd2f6926fe1e4c3fb82f0
Misha Khush <Misha26099777@yandex.ru>
2021-08-27 14:46:22 +0300

    Разработал метода readFile который считывает с файла данные и сохраняет в List

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,23 +2,33 @@ package org.example;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.*;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        readFile();
+        Random random = new Random();
+        ArrayList<String> name = readFile("students.txt");
+        ArrayList<String> questions = readFile("questions.txt");
+
+
 
     }
-    public static void readFile(){
+
+    public static ArrayList<String> readFile(String file) {
+        ArrayList<String> buf = new ArrayList<>();
         try {
-            FileInputStream fis = new FileInputStream("questions.txt");
+            FileInputStream fis = new FileInputStream(file);
             InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
-            int i;
-            while ((i = isr.read()) != -1) {
-                System.out.print((char)i);
+            BufferedReader bufferedReader = new BufferedReader(isr);
+            String buffer = null;
+            while ((buffer = bufferedReader.readLine()) != null) {
+                buf.add(buffer);
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return buf;
     }
-
 }
